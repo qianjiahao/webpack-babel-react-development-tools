6bca7571a38737f3f3824041579a8ce0b538c57e
william <1041897388@qq.com>
2015-08-19 13:50:02 +0800

    specify the webpack log

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,47 @@
+var fse = require('fs-extra');
+var fs = require('fs');
+
+var basePath = './dist';
+
+fs.readdir(basePath, function(err, subPath) {
+  if(subPath && subPath.length) {
+    console.log('------------>');
+    subPath.map(function(sub) {
+      console.log('Deleting ===> ' + sub);
+    });
+    console.log('------------>');
+
+    fse.emptyDir(basePath, function(err) {
+      if(err) return err;
+    });
+  }
+});
+
+
+var entry = {
+  'index': './app/index.js',
+  'demo': './app/demo.js'
+}
+
 module.exports = {
-  entry: "./app/index.js",
+  entry: entry,
   output: {
     path: 'dist',
-    filename: "bundle.js"
+    filename: "bundle-[name].js"
   },
   resolve: {
     modulesDirectories: ['', 'node_modules','assets/styles']
   },
   module: {
     loaders: [
-      {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"},
-      {test: /\.css$/, loader: "style-loader!css-loader"}
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel?optional[]=runtime&stage=0'
+      },
+      {
+        test: /\.css$/,
+        loader: "style-loader!css-loader"}
     ]
   }
 }
